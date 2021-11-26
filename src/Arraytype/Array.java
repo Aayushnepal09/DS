dd3d186b278f1896ae5186377be1054ba4cd146a
aauysh <nepal3491@gmail.com>
2021-11-26 08:55:41 +0545

    Initial commit

---
+++ src/Arraytype/Array.java
@@ -0,0 +1,40 @@
+package Arraytype;
+import java.util.Scanner;
+
+
+public class Array {
+    public int[] insertAtAnyPosition(int[] myArray, int n, int pos, int data){
+        int newarr[]=new int[n+1];
+        for(int i=0;i<n+1;i++){
+
+            if(i<pos-1){
+                newarr[i]=myArray[i];
+            }
+            else if(i==pos-1){
+                newarr[i]=data;
+            }
+            else{
+                newarr[i]=myArray[i-1];
+            }
+
+        }
+        return newarr;
+
+    }
+
+    public static void main(String[] Args){
+        int a[] ={10,20,30,40,50};
+        System.out.println("printing old data");
+        for(int i=0;i<a.length;i++){
+            System.out.println(a[i]);
+        }
+       Array r=new Array();
+        int x[]=r.insertAtAnyPosition(a,5,3,25);
+
+        System.out.println("printing newdata");
+        for(int i=0;i<a.length;i++){
+            System.out.println(x[i]);
+        }
+
+    }
+}
---
+++ src/Arraytype/Empty.java
@@ -0,0 +1,50 @@
+package Arraytype;
+
+public class Empty {
+
+    public static int a[] = new int[6];
+    static int n = 0;
+    Empty() {
+        for (int i = 0; i < 5; i++) {
+            n++;
+            a[i] = n;
+        }
+    }
+
+    
+//add
+    static void insAtanyPos(int pos, int data) {
+
+        for (int i = n - 1; i >= pos - 1; i--) {
+            a[i + 1] = a[i];
+        }
+        a[pos - 1] = data;
+        n++;
+    }
+
+
+    //remove
+    static void RemoveAtAnyPos(int pos) {
+        for(int  i=pos-1; i<n-1;i++){
+            a[i]=a[i+1];
+        }
+        n--;
+    }
+
+
+
+
+    public static void main(String[] args) {
+        Empty c = new Empty();
+        System.out.println("printing old values");
+        for (int i = 0; i < n; i++) {
+            System.out.println(a[i]);
+        }
+        insAtanyPos(3, 8);
+        System.out.println("printing new values");
+        for (int i = 0; i < n; i++) {
+            System.out.println(a[i]);
+        }
+
+    }
+}
